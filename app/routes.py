d89322fb3842359953fba592375bc32dfa0bbdd8
NickoGG <sboopjordan@gmail.com>
2023-09-07 10:25:16 +0300

    login forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Nicko'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts = posts, title='Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    render_template('login.html', form=form, title='Sign In')
