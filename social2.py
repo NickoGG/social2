9e751acb554c4fa99afbb0bea73026bedeef959e
NickoGG <sboopjordan@gmail.com>
2024-02-16 23:51:37 +0300

    test

--- social2.py
+++ social2.py
@@ -1,6 +1,8 @@
 from app import app, db
 from app.models import User, Post
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
